9aba5381ffcb1a2fdf3650a6fa72b759a3f90043
SravyaGeethikaSala <salasravyageethika@gmail.com>
2024-10-30 16:19:41 -0500

    added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
+var computationRouter = require('./routes/computation'); // New computation route
 
 var app = express();
 
@@ -17,12 +18,13 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/mydata', mydataRouter);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', mydataRouter);
+app.use('/computation', computationRouter); // Register the computation route
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,3 +43,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+;
