817188b24e9351779a21bc3a9e38340296cede50
SravyaGeethikaSala <salasravyageethika@gmail.com>
2024-10-30 11:42:58 -0500

    updated mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET mydata page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shravya Geethika Sai Vemula' });
+  res.render('mydata', { title: 'Shravya Geethika Sai Vemula' });
 });
 
 module.exports = router;
